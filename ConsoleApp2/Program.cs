a5f9ec82bf882ea9d8bc39cb6b05b66be0de9ef7
Tadele Asitatikie <tadele.asitatikie@gmail.com>
2020-08-14 13:18:39 +0200

    a student info added and displayed

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -11,6 +11,11 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the world of GitHub Visual Studio");
+            Student s = new Student();
+            s.Name = "Abel Yilak";
+            s.dept = "Electrical Engineering";
+            Console.WriteLine("Student name     Student Department");
+            Console.WriteLine(s.Name + "    "+s.dept);
             Console.WriteLine("Press any key to continue");
             Console.ReadKey();
         }
